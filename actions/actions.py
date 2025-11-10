28032065211d67d0b220d67ae90e755a0d068668
Andrew Weir <andoweir@gmail.com>
2025-11-10 19:53:54 +0100

    Started sql

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
+import sqlite3
 
 class ActionCheckSufficientFunds(Action):
     
@@ -22,3 +23,22 @@ class ActionCheckSufficientFunds(Action):
         transfer_amount = tracker.get_slot("amount")
         has_sufficient_funds = transfer_amount <= balance
         return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
+
+class ActionGetPossibleRooms(Action):
+    def name(self) -> Text:
+        return "action_get_possible_rooms"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        number_of_customers = tracker.get_slot("booking_number")
+        conn = sqlite3.connect("db/IvyGate.db")
+        mycur = conn.cursor()
+        sql = "select name from rooms where capacity >= 2" #2 is hard coded and needs to be replaced
+        mycur.execute(sql)
+        possible_rooms = mycur.fetchone()
+        #print(possible_rooms)
+        return [SlotSet("possible_rooms", possible_rooms)]
